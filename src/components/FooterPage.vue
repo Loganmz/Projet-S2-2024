42ba024a9a9e8a2e0e0867afa474ceb7a608d826
Loganmz <116796944+Loganmz@users.noreply.github.com>
2024-05-15 11:33:48 +0200

    test

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -6,75 +6,41 @@ import IconInstagram from '@/components/icons/IconInstagram.vue'
 import IconTwitter from '@/components/icons/IconTwitter.vue'
 import IconLinkedin from '@/components/icons/IconLinkedin.vue'
 import IconTiktok from '@/components/icons/IconTiktok.vue'
-import IconLogo from '@/components/icons/iconLogo.vue'
+import IconLogo from '@/components/icons/IconLogo.vue'
 import { RouterLink } from 'vue-router'
 
 
 </script>
 
 <template>
-   <footer class="bg-sky-700 container space-y-10 pt-20 lg:grid lg:grid-cols-4 lg:gap-8 lg:space-y-0">
+   <footer class="bg-gradient-to-b from-cyan-200 to-sky-700 container space-y-10 pt-20 lg:grid lg:grid-cols-4 lg:gap-8 lg:space-y-0">
     <IconLogo class=" mx-auto lg:col-span-1" />
     <nav
-      class="px-5 columns-2 gap-8 space-y-8 lg:columns-3 lg:col-span-3 lg:px-0 *:break-inside-avoid *:space-y-3"
+      class="px-5 columns-1 text-center gap-8 space-y-8 lg:columns-2 lg:col-span-3 lg:px-0 *:break-inside-avoid *:space-y-3 lg:text-left"
     >
       <div>
-        <h3 class="uppercase text-sm lg:text-base">Sell home</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/calm">session calme</RouterLink></li>
-          <li><RouterLink to="/memory">See a memory</RouterLink></li>
-          <li><RouterLink to="/quote">See inspiring quote</RouterLink></li>
-          <li><RouterLink to="/stories">Create a memory</RouterLink></li>
-        </ul>
-      </div>
-      <div>
-        <h3 class="uppercase text-sm lg:text-base">Buy a home</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/buy">Buy</RouterLink></li>
-          <li><RouterLink to="/finance">Finance</RouterLink></li>
-        </ul>
-      </div>
-
-      <div>
-        <h3 class="uppercase text-sm lg:text-base">Buy, rent and sell</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/buy-and-sell">Buy and sell properties</RouterLink></li>
-          <li><RouterLink to="/rent">Rent home</RouterLink></li>
-          <li><RouterLink to="/builder">Builder trade-up</RouterLink></li>
-        </ul>
-      </div>
-      <div>
-        <h3 class="uppercase text-sm lg:text-base">Terms &amp; privacy</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/trust">Trust & Safety</RouterLink></li>
-          <li><RouterLink to="/terms">Terms of Service</RouterLink></li>
-          <li><RouterLink to="/privacy">Privacy Policy</RouterLink></li>
-        </ul>
-      </div>
-
-      <div>
-        <h3 class="uppercase text-sm lg:text-base">About</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/company">Company</RouterLink></li>
-          <li><RouterLink to="/how-it-works">How it works</RouterLink></li>
+        <ul class="text-white font-bold text-base lg:text-base">
+          <li><RouterLink to="/sessionCalme">Session calme</RouterLink></li>
+          <li><RouterLink to="/voirSouvenir">Voir un souvenir</RouterLink></li>
+          <li><RouterLink to="/voirCitation">Voir une citation inspirante</RouterLink></li>
+          <li><RouterLink to="/creerSouvenir">Créer un souvenir</RouterLink></li>
           <li><RouterLink to="/contact">Contact</RouterLink></li>
-          <li><RouterLink to="/investors">Investors</RouterLink></li>
+
         </ul>
       </div>
       <div class="break-inside-avoid space-y-3">
-        <h3 class="uppercase text-sm lg:text-base">Resources</h3>
-        <ul class="text-gray-400 text-sm lg:text-base">
-          <li><RouterLink to="/blog">Blog</RouterLink></li>
-          <li><RouterLink to="/guides">Guides</RouterLink></li>
-          <li><RouterLink to="/faq">FAQ</RouterLink></li>
-          <li><RouterLink to="/help">Help Center</RouterLink></li>
+        <ul class="text-white text-sm lg:text-base">
+          <li><RouterLink to="/mentionLegal">Mentions légales</RouterLink></li>
+          <li><RouterLink to="/politiqueConfidentialite">Politique de confidentialité</RouterLink></li>
+          <li><RouterLink to="/cgu">Conditions générale d'utilisations</RouterLink></li>
+          <li><RouterLink to="/cookies">Help Center</RouterLink></li>
         </ul>
       </div>
     </nav>
     <div
-      class="px-5 text-gray-400 border-t border-gray-300 py-8 space-y-6 text-center lg:col-span-4 lg:flex lg:justify-around lg:items-center lg:space-y-0"
+      class="px-5 text-white py-8 space-y-6 text-center lg:col-span-4 lg:flex lg:justify-around lg:items-center lg:space-y-0"
     >
-      <p>©2021 Estatery. All rights reserved</p>
+      <p>©2024 PuryMind All rights reserved</p>
       <ul class="flex gap-12 justify-center">
         <li>
           <a href="#" aria-label="Facebook"
