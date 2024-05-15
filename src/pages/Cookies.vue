b9c1d2ef5456d59185d793b87829f5736fd11d03
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2024-05-15 11:17:35 +0200

    pages mentions légale

--- src/pages/Cookies.vue
+++ src/pages/Cookies.vue
@@ -17,6 +17,36 @@
         <li>Le site Web fait référence à PuryMind, accessible à partir de https://purymind.fr</li>
         <li>“Vous” signifie les  personnes qui accèdent au site Web ou l'utilisent, ou une entreprise, ou toute entité juridique au nom de laquelle cette personne accède au site Web ou l’utilise, selon le cas.</li>
     </ul>
+    <h2>2. L’utilisation des cookies</h2>
+    <h3>2.1. Types de cookies que nous utilisons</h3>
+    <p>Les cookies peuvent être des cookies "permanents" ou "de session". Les cookies permanents restent sur votre ordinateur personnel ou votre appareil mobile lorsque vous vous déconnectez, tandis que les cookies de session sont supprimés dès que vous fermez votre navigateur Web.</p>
+    <p>Nous utilisons des cookies de session et des cookies permanents pour les fins énoncées ci-dessous :</p>
+    <h3>2.2. Cookies nécessaires/essentiels </h3>
+    <p>Type: Cookies de session</p>
+    <p>Administrés par : Le groupe PuryMind</p>
+    <p>Objectif : Les cookies de session sont utilisés pour stocker des informations sur la session en cours. Ces cookies sont essentiels pour vous fournir des services disponibles sur le site Web et pour vous permettre d’utiliser certaines de ses fonctionnalités. Ils aident à authentifier les utilisateurs et à prévenir l’utilisation frauduleuse des comptes utilisateurs. Sans ces cookies, les services que vous avez demandés ne peuvent pas être fournis, et nous utilisons uniquement ces cookies pour vous fournir ces services. D'autres informations, telles que les données de connexion ou les formulaires en ligne déjà remplis, sont également conservées pendant la session. Toutefois, si vous mettez fin à votre session de navigation, l’identifiant de session et toutes les autres données stockées seront effacés. Cela signifie que lorsque vous ouvrez la même page Web dans une nouvelle fenêtre de navigateur, vous serez considéré comme un nouveau visiteur.</p>
+    <h3>2.3. Cookies de fonctionnalité</h3>
+    <p>Type: Cookies permanents</p>
+    <p>Administrés par : Le groupe PuryMind</p>
+    <p>Objectif : Un cookie permanent est un cookie qui reste sur le disque de l'internaute d'une session de navigation à l'autre. La terme est cependant un peu trompeur, car le plus souvent le cookie permanent à une durée de vie limitée dans le temps qui est spécifiée par l'organisation qui l'a placé et qui dépend de l'usage qui en est fait. Ces Cookies nous permettent de nous souvenir des choix que vous faites lorsque vous utilisez le site, tels que la mémorisation de vos informations de connexion ou de votre préférence linguistique. Le but de ces Cookies est de vous fournir une expérience plus personnelle et de vous éviter d’avoir à ressaisir vos préférences à chaque fois que vous utilisez le Site. Les cookies permanents permettent notamment de reconnaître l'internaute, ou plutôt son ordinateur, d'une session de visite d'un site web à l'autre ou d'une exposition publicitaire à l'autre.</p>
+    <h3>2.4. Vos choix concernant les cookies</h3>
+    <p>Si vous préférez éviter l'utilisation des cookies sur notre site Web, vous pouvez désactiver les cookies dans votre navigateur et supprimer les cookies enregistrés. Toutefois, veuillez noter que cela pourrait affecter certaines fonctionnalités du site.</p>
+    <p>Si vous souhaitez supprimer les cookies ou demander à votre navigateur Web de les supprimer ou de les refuser, veuillez consulter les pages d’aide de votre navigateur Web.</p>
+    <p>Pour le navigateur Web Chrome, veuillez visiter cette page à partir de Google : https://support.google.com/accounts/answer/32050</p>
+    <p>Pour le navigateur Web Internet Explorer, veuillez visiter cette page à partir de Microsoft : http://support.microsoft.com/kb/278835</p>
+    <p>Pour le navigateur Web Firefox, veuillez visiter cette page à partir de Mozilla : https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored</p>
+    <p>Pour le navigateur Web Firefox, veuillez visiter cette page à partir de Mozilla : https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored</p>
+    <p>Pour le navigateur Web Safari, veuillez visiter cette page à partir d’Apple : https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac</p>
+    <p>Pour tout autre navigateur Web, veuillez visiter les pages Web officielles de votre navigateur Web.</p>
+    <h3>Plus d’informations sur les cookies</h3>
+    <p>Vous pouvez en savoir plus sur notre politique d’utilisation des Cookies, veuillez nous contacter ou visiter les pages Web officielles de notre site Web.</p>
+    <h3>2.6. Contactez-nous</h3>
+    <p>Si vous avez des questions au sujet de la présente politique sur les cookies, vous pouvez communiquer avec nous :</p>
+    <p>Par email : purymind@entreprise.fr</p>
+    <p>Par téléphone : XX XX XX XX XX</p>
+    <p>Par courrier : Cr Louis Leprince-Ringuet, Montbéliard, Bourgogne-Franche-Comté, 25200, France </p>
+    <p>Cette politique des cookies a été conçue pour vous informer de manière claire et concise. Nous vous recommandons de consulter régulièrement cette politique pour vous tenir informé des éventuelles mises à jour.</p>
+
 </div>
 
 </template>
---
+++ src/pages/cgv.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+<h1>Conditions Générales de Vente</h1>
+<div>
+    <p>Date de Dernière Mise à Jour : 08/03/2024</p>
+    <h2>1. Champ d'application</h2>
+    <p>Les présentes Conditions Générales de Vente (CGV) régissent la souscription et l'utilisation des services Premium proposés par PuryMind. Toute souscription à un abonnement Premium implique l'acceptation sans réserve de ces CGV.</p>
+    <h2>2. Abonnements premium</h2>
+    <p>PuryMind propose des abonnements Premium donnant accès à des fonctionnalités exclusives. Les tarifs, durées et conditions des abonnements sont détaillés sur le site. En souscrivant à un abonnement, l'utilisateur accepte les conditions spécifiques liées à celui-ci.</p>
+    <h2>3. Processus de commande et paiement</h2>
+    <p>La commande d'un abonnement Premium s'effectue en ligne sur le site. Le paiement peut être effectué par carte bancaire, virement bancaire SEPA, PayPal, iTunes (Apple), Google Play Store (Android), ou Huawei Pay (Huawei). Les coordonnées bancaires ne sont pas conservées par PuryMind. Le paiement est dû à la souscription et renouvelé automatiquement selon la durée de l'abonnement.</p>
+    <h2>4. Modalités de paiement</h2>
+    <p>Les abonnements mensuels sont payables par prélèvement automatique. En cas de reconduction tacite, le tarif en vigueur sera appliqué. Vous devez informer rapidement PuryMind de tout changement de mode de paiement.</p>
+    <h2>5. Autorisation de prélèvement</h2>
+    <p>Vous autorisez PuryMind et le prestataire de paiement à débiter le moyen de paiement utilisé pour l'abonnement initial lors des renouvellements. En cas d'impossibilité de prélèvement, vous restez redevable des échéances précédentes.</p>
+    <h2>6. Facturation et renouvellement</h2>
+    <p>La facturation est effectuée au début de chaque période d'abonnement. En cas de reconduction tacite, l'utilisateur sera informé à l'avance du renouvellement. En cas d'impossibilité de prélèvement, l'accès aux services Premium peut être suspendu.</p>
+    <h2>7. Droit de rétractation</h2>
+    <p>Conformément à la réglementation, l'utilisateur dispose d'un délai de rétractation de 14 jours à compter de la confirmation de la commande pour annuler son abonnement et demander un remboursement.</p>
+    <h2>8. Résiliation de l'abonnement</h2>
+    <p>L'utilisateur peut résilier son abonnement Premium à tout moment. La résiliation prend effet à la fin de la période en cours. Aucun remboursement n'est effectué pour la période en cours.</p>
+    <h2>9. Responsabilité et garanties</h2>
+    <p>PuryMind s'engage à fournir les services Premium conformément aux descriptions fournies. En cas de dysfonctionnement, l'utilisateur peut contacter le support client. PuryMind ne peut être tenu responsable des interruptions de service pour des raisons indépendantes de sa volonté.</p>
+    <h2>10. Modifications des CGV</h2>
+    <p>PuryMind se réserve le droit de modifier les présentes CGV. Les utilisateurs seront informés des changements par email. En cas de désaccord, l'utilisateur peut résilier son abonnement.</p>
+    <h2>11. Données personnelles</h2>
+    <p>Les données personnelles collectées dans le cadre de la souscription sont traitées conformément à la Politique de Confidentialité de PuryMind.</p>
+    <h2>12. Litiges et droit applicable</h2>
+    <p>En cas de litige, les parties s'efforceront de trouver une solution amiable. À défaut, les tribunaux français seront compétents. Les présentes CGV sont soumises au droit français.</p>
+    <p>Pour toute question ou préoccupation concernant votre abonnement Premium, veuillez contacter notre service clientèle à l'adresse purymind.help@gmail.com.
+Merci d'avoir pris le temps de lire nos Conditions Générales de Vente. En souscrivant à l'abonnement Premium, vous reconnaissez avoir lu, compris et accepté ces termes.</p>
+</div>
+</template>
