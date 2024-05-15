2e898414bcfa4662b5393254eb9c683bd45b0eba
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-05-15 14:43:41 +0200

    html/css page mentions legales

--- src/pages/mentionlegal.vue
+++ src/pages/mentionlegal.vue
@@ -3,31 +3,31 @@
 </script>
 
 <template>
-  <h1> Mentions Légales </h1>
-  <div>
-  <p>Date de la dernière mise à jour : 10/04/2024</p>
-  <h2>Éditeur du site</h2>
-  <p>Le  site est édité par : </p>
-  <p> PuryMind, société au capital de 3 euros, inscrite au R.C.S. de BELFORT sous le numéro RCS MONTBELIARD A XXX XXX XXX, dont le siège social est situé à Cours Louis Leprince-Ringuet, Montbéliard, Bourgogne-Franche-Comté, 25200, France, représenté(e) par Le groupe PuryMind dûment habilité(e). </p>
-  <p>Le numéro individuel TVA de l’éditeur est : FR XX XXXXXXXXX.</p>
-  <h2>Directeur de la publication</h2>
-  <p>Le Directeur de la publication du Site est PuryMind</p>
-  <h2>Hébergeur du site</h2>
-  <p>Le Site est hébergé par la société Infomaniak, située Rue Eugène-Marziano 25, 1227 Genève, Suisse, (contact téléphonique ou email : (+41) 22 820 35 44).</p>
-  <p>La sécurité de vos données est notre priorité. Le site est hébergé de manière sécurisée afin de garantir la protection des données.</p>
-  <h2>Nous contacter</h2>
-  <p>Par téléphone : +33XXXXXXXXX </p>
-  <p>Par email : purymind@entreprise.fr  </p>
-  <p>Par courrier : Cours Louis Leprince-Ringuet, 25200 Montbéliard</p>
-<h2>Propriété intellectuelle</h2>
-<p>L'ensemble du contenu de l'application "PuryMind" (textes, images, vidéos, etc.) est la propriété exclusive de PuryMind. Toute reproduction, distribution, ou utilisation sans autorisation écrite est strictement interdite. Des conséquences légales peuvent découler de toute violation du droit d'auteur.</p>
-<h2>Collecte de données personnelles</h2>
-<p> L'application PuryMind peut collecter des données personnelles dans le cadre de son fonctionnement. Ces données sont traitées conformément à la législation en vigueur sur la protection des données personnelles. L'utilisateur consent à cette collecte et peut en apprendre davantage en consultant notre politique de confidentialité.</p>
-<h2>Cookies</h2>
-<p>L'application "PuryMind" peut utiliser des cookies pour améliorer l'expérience utilisateur. Si il veut plus d’informations sur les cookies, nous vous invitons à lire la politique des cookies. L'utilisateur a la possibilité de configurer son navigateur pour désactiver les cookies s'il le souhaite.</p>
-<h2>Responsabilité</h2>
-<p>L'utilisation de l'application "PuryMind" relève de la responsabilité de l'utilisateur. PuryMind ne peut être tenue responsable des dommages directs ou indirects résultant de l'utilisation de l'application.</p>
-<h2>Modifications des mentions légales</h2>
-<p>PuryMind se réserve le droit de modifier les présentes mentions légales à tout moment. Les utilisateurs seront notifiés des changements par le biais de l'application.</p>
+  <h1 class="text-3xl font-bold m-8 text-center lg:text-4xl">Mentions légales</h1>
+  <div class="m-8 lg:mx-40">
+  <p class="text-base lg:text-lg">Date de la dernière mise à jour : 10/04/2024</p>
+  <h2 class="text-xl font-bold text-sky-400	my-4 	">Éditeur du site</h2>
+  <p class="text-base lg:text-lg">Le site est édité par : </p>
+  <p class="text-base lg:text-lg"> PuryMind, société au capital de 3 euros, inscrite au R.C.S. de BELFORT sous le numéro RCS MONTBELIARD A XXX XXX XXX, dont le siège social est situé à Cours Louis Leprince-Ringuet, Montbéliard, Bourgogne-Franche-Comté, 25200, France, représenté(e) par Le groupe PuryMind dûment habilité(e). </p>
+  <p class="text-base lg:text-lg">Le numéro individuel TVA de l’éditeur est : FR XX XXXXXXXXX.</p>
+  <h2 class="text-xl font-bold text-sky-400 my-4	">Directeur de la publication</h2>
+  <p class="text-base lg:text-lg">Le Directeur de la publication du Site est PuryMind</p>
+  <h2 class="text-xl font-bold text-sky-400 my-4	">Hébergeur du site</h2>
+  <p class="text-base lg:text-lg">Le Site est hébergé par la société Infomaniak, située Rue Eugène-Marziano 25, 1227 Genève, Suisse, (contact téléphonique ou email : (+41) 22 820 35 44).</p>
+  <p class="text-base lg:text-lg">La sécurité de vos données est notre priorité. Le site est hébergé de manière sécurisée afin de garantir la protection des données.</p>
+  <h2 class="text-xl font-bold text-sky-400 my-4	">Nous contacter</h2>
+  <p class="text-base lg:text-lg">Par téléphone : +33XXXXXXXXX </p>
+  <p class="text-base lg:text-lg">Par email : purymind@entreprise.fr  </p>
+  <p class="text-base lg:text-lg">Par courrier : Cours Louis Leprince-Ringuet, 25200 Montbéliard</p>
+  <h2 class="text-xl font-bold text-sky-400	my-4">Propriété intellectuelle</h2>
+  <p class="text-base lg:text-lg">L'ensemble du contenu de l'application "PuryMind" (textes, images, vidéos, etc.) est la propriété exclusive de PuryMind. Toute reproduction, distribution, ou utilisation sans autorisation écrite est strictement interdite. Des conséquences légales peuvent découler de toute violation du droit d'auteur.</p>
+  <h2 class="text-xl font-bold text-sky-400 my-4	">Collecte de données personnelles</h2>
+  <p class="text-base lg:text-lg"> L'application PuryMind peut collecter des données personnelles dans le cadre de son fonctionnement. Ces données sont traitées conformément à la législation en vigueur sur la protection des données personnelles. L'utilisateur consent à cette collecte et peut en apprendre davantage en consultant notre politique de confidentialité.</p>
+  <h2 class="text-xl font-bold text-sky-400	my-4">Cookies</h2>
+  <p class="text-base lg:text-lg">L'application "PuryMind" peut utiliser des cookies pour améliorer l'expérience utilisateur. Si il veut plus d’informations sur les cookies, nous vous invitons à lire la politique des cookies. L'utilisateur a la possibilité de configurer son navigateur pour désactiver les cookies s'il le souhaite.</p>
+  <h2 class="text-xl font-bold text-sky-400	my-4">Responsabilité</h2>
+  <p class="text-base lg:text-lg">L'utilisation de l'application "PuryMind" relève de la responsabilité de l'utilisateur. PuryMind ne peut être tenue responsable des dommages directs ou indirects résultant de l'utilisation de l'application.</p>
+  <h2 class="text-xl font-bold text-sky-400 my-4">Modifications des mentions légales</h2>
+  <p class="text-base lg:text-lg">PuryMind se réserve le droit de modifier les présentes mentions légales à tout moment. Les utilisateurs seront notifiés des changements par le biais de l'application.</p>
   </div>
 </template>
