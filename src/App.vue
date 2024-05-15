292b6bd7bf0af4bfb5bee91188fb113e87fad21b
Loganmz <116796944+Loganmz@users.noreply.github.com>
2024-05-15 09:04:41 +0200

    footer-step1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onErrorCaptured } from 'vue'
+import { RouterLink, RouterView } from 'vue-router/auto'
+import FooterPage from './components/FooterPage.vue'
+
+onErrorCaptured((err, instance, info) => {
+  console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
+  return true
+})
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <Suspense>
+      <component :is="Component" />
+    </Suspense>
+  </RouterView>
+  <FooterPage />
+</template>
