719ca4e4aec1be7af505e9ce4f81cced21319b91
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-05-29 17:02:59 +0200

    page inscription + connexion

---
+++ pocketbase/pb_migrations/1716985802_updated_users.js
@@ -0,0 +1,49 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("0bv3audc")
+
+  // remove
+  collection.schema.removeField("2qg1d0m1")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "0bv3audc",
+    "name": "adresse_mail",
+    "type": "email",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "exceptDomains": null,
+      "onlyDomains": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2qg1d0m1",
+    "name": "mots_de_passe",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
--- src/backend.ts
+++ src/backend.ts
@@ -3,4 +3,21 @@ import { type TypedPocketBase } from './pocketbase-types.js'
 
 export const pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase
 
-// Copier ici les fonctions developpées en R214 | Système d'information
+
+export async function Adduser(event: Object) {
+   try {
+       const record = await pb.collection('users').create(event);
+       return record;
+   } catch (error) {
+       return error;
+   }
+}
+
+export async function signIn(user: { email: string, password: string }) {
+  try {
+    const record = await pb.authStore.signIn(user);
+    return record;
+  } catch (error) {
+    return error;
+  }
+}
