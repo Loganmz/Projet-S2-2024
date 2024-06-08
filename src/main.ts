e33e22cedf64533b48650b40625b6064f42f9871
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-06-08 17:44:31 +0200

    modif

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import V3ScrollLock from 'v3-scroll-lock'
-
-const router = createRouter({
-  history: createWebHistory()
-  // the routes property is handled by the plugin
-})
 
 const app = createApp(App)
 
-app.use(router)
-app.use(V3ScrollLock, {})
+app.use(
+  createRouter({
+    history: createWebHistory()
+  })
+)
 
 app.mount('#app')
--- src/pages/Inscription.vue
+++ src/pages/Inscription.vue
@@ -4,6 +4,7 @@ import { Adduser } from '../backend'
 import { useRouter } from 'vue-router'
 import Button from '../components/Button.vue'
 
+
 const router = useRouter()
 
 const polconfident = ref(false)
